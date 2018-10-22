58636b128437189510ea9f54fe7c322d9c8ce853
Basim Al-Jawahery <basimdev@gmail.com>
2018-10-22 07:36:53 +0300

    Protected property and tests for it  were implemented in DerivedClass

--- es5-derived.js
+++ es5-derived.js
@@ -7,27 +7,23 @@ function DerivedClass() {
         return new DerivedClass();
     
     var _self = this;
+    var _baseProtectedPropertyDescriptor = Object.getOwnPropertyDescriptor(this, 'protectedProperty');
     var _base = {
         /** Get protectedMethod from BaseClass and bind it into instance of DerivedClass. */
         protectedMethod: _self.protectedMethod.bind(_self),
-        /** Get protectedProperty from BaseClass and bind it into instance of DerivedClass. */
-        //protectedProperty: this.protectedProperty.bind(this)
     };
 
-    // Object.defineProperty(_base, 'protectedProperty', {
-    //     get: function() {
-    //         checkAccess.call(_self);
-    //         return _self.protectedProperty;
-    //     },
-    //     set: function(value){ 
-    //         checkAccess.call(_self);
-    //         _self.protectedProperty = value; 
-    //     }
-    // })
+    Object.defineProperty(_base, 'protectedProperty', {
+        get: function() {
+            return _baseProtectedPropertyDescriptor.get.call(_self);
+        },
+        set: function(value){ 
+            _baseProtectedPropertyDescriptor.set.call(_self, value);
+        }
+    })
 
     /** @summary Check context that is called from derived classes */
     function checkAccess() {
-        console.log('DerivedClass.checkAccess: this.constructor = ', this.constructor.name);
         if (this.constructor === DerivedClass)
             throw new ProtectedError();
     }
@@ -37,43 +33,42 @@ function DerivedClass() {
         enumerable: false,
         // Do checks and then call protectedMEthod from BaseClass
         value: function () {
-            console.log('Derived.protectedMethod: this = ' + this.constructor.name);
-            checkAccess.call(_self); // Важен _self, 
+            checkAccess.call(_self); 
             return _base.protectedMethod();
         }
     });
 
     /** @description It is vitally important to redifne the property in DerivedClass
-     * and check the access here. Because only here we can forbid access from such a case:
+     * and check the access here. Because only here we can forbid access from 
+     * using protectedProperty as a public member of DerivedClass instance: e.g.
      * var obj = new DeriverClass();
      * obj.protectedProperty = 12; 
-     * Therefore, we must check access here in the DerivedClass.
      * */
-    // Object.defineProperty(_self, 'protectedProperty', {
-    //     get: function () {
-    //         checkAccess.call(_self);
-    //         return base.protectedProperty;
-    //     },
-    //     set: function (value) {
-    //         checkAccess.call(_self);
-    //         _base.protectedProperty = value;
-    //     },
-    //     enumerable: false,
-    //     configurable: false
-    // });
+    Object.defineProperty(_self, 'protectedProperty', {
+        get: function () {
+            checkAccess.call(_self);
+            return base.protectedProperty;
+        },
+        set: function (value) {
+            checkAccess.call(_self);
+            _base.protectedProperty = value;
+        },
+        enumerable: false,
+        configurable: false
+    });
 
     // Use of protected method inside the DerivedClass
     this.usingMethod = function () {
         console.log(_base.protectedMethod());
     }
     // Use of protected property inside the DerivedClass
-    // this.writingToTheProperty = function () {
-    //     console.log(_base.protectedProperty);
-    // }    
+    this.writingToTheProperty = function () {
+        _base.protectedProperty = 12;
+    }    
     // Use of protected property inside the DerivedClass
-    // this.readingOfTheProperty = function () {
-    //     _base.protectedProperty = 12;
-    // }     
+    this.readingOfTheProperty = function () {
+        console.log(_base.protectedProperty);
+    }     
 }
 DerivedClass.prototype = new BaseClass();
 Object.defineProperty(DerivedClass.prototype, 'constructor', {
--- tests/es5.derived.test.js
+++ tests/es5.derived.test.js
@@ -1,7 +1,7 @@
 const ProtectedError = require('../protected-error');
 const DerivedClass = require('../es5-derived');
 
-test('should not allow to access protectedMethod', () => {
+test('should not allow to access protectedMethod: protectedMethod as a public member of DerivedClass', () => {
     var obj = new DerivedClass();
     expect(obj.protectedMethod).toThrow(ProtectedError);
 });
@@ -11,22 +11,22 @@ test('should not throw ProtectedError when protectedMethod called from inside Ba
     expect(obj.usingMethod).not.toThrow(ProtectedError);
 });
 
-// test('should not allow to read protectedProperty', () => {
-//     var obj = new DerivedClass();
-//     expect(()=>obj.protectedProperty).toThrow(ProtectedError);
-// });
+test('should not allow to read protectedProperty: protectedProperty as a public member of DerivedClass', () => {
+    var obj = new DerivedClass();
+    expect(()=>obj.protectedProperty).toThrow(ProtectedError);
+});
 
-// test('should not allow to set protectedProperty', () => {
-//     var obj = new DerivedClass();
-//     expect(()=>{obj.protectedProperty = 100;}).toThrow(ProtectedError);
-// });
+test('should not allow to set protectedProperty: protectedProperty as a public member of DerivedClass', () => {
+    var obj = new DerivedClass();
+    expect(()=>{obj.protectedProperty = 100;}).toThrow(ProtectedError);
+});
 
-// test('should not throw ProtectedError when writing to protectedProperty used inside BaseClass functions', () => {
-//     var obj = new DerivedClass();
-//     expect(obj.writingToTheProperty).not.toThrow(ProtectedError);
-// });
+test('should not throw ProtectedError when writing to protectedProperty used inside BaseClass functions', () => {
+    var obj = new DerivedClass();
+    expect(obj.writingToTheProperty).not.toThrow(ProtectedError);
+});
 
-// test('should not throw ProtectedError when reading protectedProperty used inside BaseClass functions', () => {
-//     var obj = new DerivedClass();
-//     expect(obj.readingOfTheProperty).not.toThrow(ProtectedError);
-// });
+test('should not throw ProtectedError when reading protectedProperty used inside BaseClass functions', () => {
+    var obj = new DerivedClass();
+    expect(obj.readingOfTheProperty).not.toThrow(ProtectedError);
+});
